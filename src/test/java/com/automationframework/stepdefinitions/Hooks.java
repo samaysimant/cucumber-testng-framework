5fd1623ae6d92ae7d33cc82cbf6c71bc7755c69b
sbarik <sbarik@NSWSYDNLP213.planit.local>
2018-07-17 12:59:49 +1000

    u

--- src/test/java/com/automationframework/stepdefinitions/Hooks.java
+++ src/test/java/com/automationframework/stepdefinitions/Hooks.java
@@ -1,10 +1,14 @@
 package com.automationframework.stepdefinitions;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import com.automationframeworks.Utility.ExcelReader;
 import com.automationframeworks.Utility.WebdriverManager;
@@ -14,6 +18,7 @@ import com.cucumber.listener.Reporter;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
+import io.appium.java_client.android.AndroidDriver;
 
 /**
  * Hello world!
@@ -28,9 +33,12 @@ public class Hooks
 	public static final long IMPLICIT_WAIT=30;
 	
     @Before
-    public void before(Scenario scenario) {
+    public void before(Scenario scenario) throws MalformedURLException {
     	System.out.println("Initiate Driver");
-    	driver=WebdriverManager.intializeDriver();
+    	//driver=WebdriverManager.intializeDriver();
+    	DesiredCapabilities capabilities = new DesiredCapabilities();
+        URL appiumURL = new URL("http://127.0.0.1:4723/wd/hub");
+        driver = new AndroidDriver<WebElement>(appiumURL, capabilities);
     	System.out.println("Current scenario:"+scenario.getName());
     	try {
 			ExcelReader.feedInputData(scenario.getName());
